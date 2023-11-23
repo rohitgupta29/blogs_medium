1a6f7ed86b318f17e2fab46027b02f17437152f1
Rohit Gupta <64639204+rohitgupta29@users.noreply.github.com>
2023-11-23 13:34:11 +0530

    Create dag.py

---
+++ Mastering Apache Airflow/dag.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'example_dag',
+    default_args=default_args,
+    description='A simple example DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+task1 = DummyOperator(task_id='task1', dag=dag)
+task2 = DummyOperator(task_id='task2', dag=dag)
+task3 = DummyOperator(task_id='task3', dag=dag)
+
+task1 >> [task2, task3]
